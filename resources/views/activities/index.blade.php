1bacaa002e1b29271a44ed9a5b442c3f5480fa51
Manuel Martínez <manuel.mtzv816@gmail.com>
2023-03-08 17:30:49 -0600

    Realizando reacomodo de carpetas y clases css

--- resources/views/activities/index.blade.php
+++ resources/views/activities/index.blade.php
@@ -2,32 +2,40 @@
   <div class="feed container">
     <div class="feed__container">
       <aside class="feed__aside">
-        <div class="feed__section">
-          <h2 class="feed__title">
+        <div class="notifications">
+          <h2 class="notifications__title">
             Notifications
           </h2>
-          <div class="feed__notifications">
-
+          <div class="notifications__container">
+            @foreach ($notifications as $notification)
+              @include('activities.notification');
+            @endforeach
           </div>
         </div>
 
-        <div class="feed__section">
-          <h2 class="feed__title">
-            Following
+        <div class="friends">
+          <h2 class="friends__title">
+            Friends
           </h2>
-          <div class="feed__following">
-
+          <div class="friends__container">
+            @foreach ($friends as $friend)
+              @include('activities.friend');
+            @endforeach
           </div>
         </div>
       </aside>
 
-      <div class="feed__section">
-        <h2 class="feed__title">
-          Activities
-        </h2>
+      <div class="feed__main">
+        <div class="activities">
+          <h2 class="activities__title">
+            Activities
+          </h2>
 
-        <div class="feed__activities">
-
+          <div class="activities__container">
+            @foreach ($notifications as $notification)
+              @include('activities.activity');
+            @endforeach
+          </div>
         </div>
       </div>
     </div>
