90de659d309fdda5802d6fe870a0ee57a4fcafdc
Manuel Martínez <manuel.mtzv816@gmail.com>
2023-03-28 18:50:57 -0600

    Agregando relaciones

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -10,6 +10,11 @@ class Activity extends Model
 {
   use HasFactory;
 
+  public function comments()
+  {
+    return $this->hasMany(Comment::class);
+  }
+
   public function users()
   {
     return $this->belongsTo(User::class);
