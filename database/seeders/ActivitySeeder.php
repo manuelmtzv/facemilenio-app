7aecf7ce7502b099b902bba275523018055d5081
Manuel Martínez <manuel.mtzv816@gmail.com>
2023-03-30 17:36:52 -0600

    Mejores a la vista de landing}

--- database/seeders/ActivitySeeder.php
+++ database/seeders/ActivitySeeder.php
@@ -18,5 +18,10 @@ class ActivitySeeder extends Seeder
       'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro molestias error autem quidem repellendus ratione libero maxime sint hic ut? Saepe, rem harum dolores distinctio iure consequuntur fugiat consectetur eligendi.',
       'user_id' => 1
     ]);
+
+    Activity::create([
+      'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro molestias error autem quidem repellendus ratDonec dignissim tortor ligula, ut placerat tortor gravida sit amet. Integer dapibus massa et erat porta condimentum. Mauris vel velit et orci ultricies porttitor sit amet vel lorem. Integer eget varius est, a tincidunt erat. Curabitur eu vehicula ipsum. Maecenas et tortor facilisis, ultrices lacus sed, egestas massa. ',
+      'user_id' => 2
+    ]);
   }
 }
